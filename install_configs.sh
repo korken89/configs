012e8bd95679134ea1b573217630d783412c7d2d
Emil Fresk <emil.fresk@gmail.com>
2019-01-31 16:22:26 +0100

    Updated some configs with correct paths, added a script to link all the configs

---
+++ install_configs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Make needed dirs
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/dunst
+mkdir -p ~/.config/fish
+
+# Link the configs
+ln -sf "$(pwd)/i3_config" ~/.config/i3/config
+ln -sf "$(pwd)/i3blocks.conf" ~/.config/i3/i3blocks.conf
+ln -sf "$(pwd)/rc_additions" ~/.rc_additions
+ln -sf "$(pwd)/aliases" ~/.aliases
+ln -sf "$(pwd)/zshrc" ~/.zshrc
+ln -sf "$(pwd)/config.fish" ~/.config/fish/config.fish
+ln -sf "$(pwd)/Xresources" ~/.Xresources
+ln -sf "$(pwd)/dunstrc" ~/.config/dunst/dunstrc
+ln -sf "$(pwd)/latexmkrc" ~/.latexmkrc
+ln -sf "$(pwd)/gitconfig" ~/.gitconfig
+ln -sf "$(pwd)/gdbinit" ~/.gdbinit
+
+# Extend .bashrc
+echo "[[ -f '$(pwd)/rc_additions' ]] && source $(pwd)/rc_additions " >> ~/.bashrc
+echo "[[ -f '$(pwd)/aliases' ]] && source $(pwd)/aliases " >> ~/.bashrc
+echo "fish " >> ~/.bashrc
