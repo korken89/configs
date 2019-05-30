3dbd9f6ebde04493a53a09278b2d7bdbc4233d48
Emil Fresk <emil.fresk@gmail.com>
2019-05-30 12:18:00 +0200

    Added Sway config

--- install_configs.sh
+++ install_configs.sh
@@ -2,11 +2,13 @@
 
 # Make needed dirs
 mkdir -p ~/.config/i3
+mkdir -p ~/.config/sway
 mkdir -p ~/.config/dunst
 mkdir -p ~/.config/fish
 
 # Link the configs
 ln -sf "$(pwd)/i3_config" ~/.config/i3/config
+ln -sf "$(pwd)/i3_config" ~/.config/sway/config
 ln -sf "$(pwd)/i3-battery.py" ~/.config/i3/i3-battery.py
 ln -sf "$(pwd)/i3blocks.conf" ~/.config/i3/i3blocks.conf
 ln -sf "$(pwd)/alacritty.yml" ~/.config/alacritty/alacritty.yml
