314f24127d2dc1f740cde8baf0cbc5bc80944005
Emil Fresk <emil.fresk@gmail.com>
2019-08-19 11:04:56 +0200

    Added script for default applications

---
+++ set_default_applications.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+xdg-mime default firefox.desktop text/html
+xdg-mime default firefox.desktop text/xml
+xdg-mime default firefox.desktop application/xhtml+xml
+xdg-mime default firefox.desktop x-scheme-handler/http
+xdg-mime default firefox.desktop x-scheme-handler/https
+xdg-mime default firefox.desktop application/x-xpinstall
+
+xdg-mime default inkscape.desktop image/svg+xml
+xdg-mime default inkscape.desktop image/svg+xml-compressed
+xdg-mime default inkscape.desktop image/x-eps
+
+xdg-mime default kicad.desktop application/x-kicad-project
+xdg-mime default pcbnew.desktop application/x-kicad-pcb
+xdg-mime default eeschema.desktop application/x-kicad-schematic
+xdg-mime default gerbview.desktop application/x-gerber
+xdg-mime default gerbview.desktop application/x-excellon
+
+xdg-mime default okularApplication_pdf.desktop application/pdf
+xdg-mime default okularApplication_pdf.desktop application/x-gzpdf
+xdg-mime default okularApplication_pdf.desktop application/x-bzpdf
+xdg-mime default okularApplication_pdf.desktop application/x-wwf
+
+xdg-mime default org.sigrok.PulseView.desktop application/vnd.sigrok.session
+
+xdg-mime default skypeforlinux.desktop x-scheme-handler/skype
+
+xdg-mime default spotify.desktop x-scheme-handler/spotify
+
+xdg-mime default telegramdesktop.desktop x-scheme-handler/tg
+
+xdg-mime default Thunar-folder-handler.desktop inode/directory
+
+xdg-mime default viewnior.desktop image/bmp
+xdg-mime default viewnior.desktop image/gif
+xdg-mime default viewnior.desktop image/jpeg
+xdg-mime default viewnior.desktop image/jpg
+xdg-mime default viewnior.desktop image/pjpeg
+xdg-mime default viewnior.desktop image/png
+xdg-mime default viewnior.desktop image/tiff
+xdg-mime default viewnior.desktop image/x-bmp
+xdg-mime default viewnior.desktop image/x-gray
+xdg-mime default viewnior.desktop image/x-icb
+xdg-mime default viewnior.desktop image/x-ico
+xdg-mime default viewnior.desktop image/x-png
+xdg-mime default viewnior.desktop image/x-portable-anymap
+xdg-mime default viewnior.desktop image/x-portable-bitmap
+xdg-mime default viewnior.desktop image/x-portable-graymap
+xdg-mime default viewnior.desktop image/x-portable-pixmap
+xdg-mime default viewnior.desktop image/x-xbitmap
+xdg-mime default viewnior.desktop image/x-xpixmap
+xdg-mime default viewnior.desktop image/x-pcx
+
+xdg-mime default vlc.desktop video/ogg
+xdg-mime default vlc.desktop video/x-ogm
+xdg-mime default vlc.desktop video/x-ogm+ogg
+xdg-mime default vlc.desktop video/x-theora+ogg
+xdg-mime default vlc.desktop video/x-theora
+xdg-mime default vlc.desktop video/x-ms-asf
+xdg-mime default vlc.desktop video/x-ms-asf-plugin
+xdg-mime default vlc.desktop video/x-ms-asx
+xdg-mime default vlc.desktop video/x-ms-wm
+xdg-mime default vlc.desktop video/x-ms-wmv
+xdg-mime default vlc.desktop video/x-ms-wmx
+xdg-mime default vlc.desktop video/x-ms-wvx
+xdg-mime default vlc.desktop video/x-msvideo
+xdg-mime default vlc.desktop video/divx
+xdg-mime default vlc.desktop video/msvideo
+xdg-mime default vlc.desktop video/vnd.divx
+xdg-mime default vlc.desktop video/avi
+xdg-mime default vlc.desktop video/x-avi
+xdg-mime default vlc.desktop video/vnd.rn-realvideo
+xdg-mime default vlc.desktop video/mp2t
+xdg-mime default vlc.desktop video/mpeg
+xdg-mime default vlc.desktop video/mpeg-system
+xdg-mime default vlc.desktop video/x-mpeg
+xdg-mime default vlc.desktop video/x-mpeg2
+xdg-mime default vlc.desktop video/x-mpeg-system
+xdg-mime default vlc.desktop video/mp4
+xdg-mime default vlc.desktop video/mp4v-es
+xdg-mime default vlc.desktop video/x-m4v
+xdg-mime default vlc.desktop video/quicktime
+xdg-mime default vlc.desktop video/x-matroska
+xdg-mime default vlc.desktop video/webm
+xdg-mime default vlc.desktop video/3gp
+xdg-mime default vlc.desktop video/3gpp
+xdg-mime default vlc.desktop video/3gpp2
+xdg-mime default vlc.desktop video/vnd.mpegurl
+xdg-mime default vlc.desktop video/dv
+xdg-mime default vlc.desktop video/x-anim
+xdg-mime default vlc.desktop video/x-nsv
+xdg-mime default vlc.desktop video/fli
+xdg-mime default vlc.desktop video/flv
+xdg-mime default vlc.desktop video/x-flc
+xdg-mime default vlc.desktop video/x-fli
+xdg-mime default vlc.desktop video/x-flv
+
